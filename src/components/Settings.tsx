9890c58cccab2288bed89db159abfe1762e7ce9c
Johan Dahl <johan.dahl.92@hotmail.com>
2022-03-03 22:47:39 +0100

    adding mui timepicker

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,11 +1,27 @@
 import React from "react"
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import TextField from '@mui/material/TextField';
+import StaticTimePicker from '@mui/lab/StaticTimePicker';
 
 export const Settings = () => {
+  const [value, setValue] = React.useState(new Date());
+
   // fetch settings from the server
   // some props on the selected time
 
   return (
-  <>
+  <>   
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <StaticTimePicker
+        displayStaticWrapperAs="mobile"
+        value={value}
+        onChange={(newValue) => {
+          setValue(newValue ?? new Date());
+        }}
+        renderInput={(params) => <TextField {...params} />}
+      />
+    </LocalizationProvider>
   </>
   )
 }
