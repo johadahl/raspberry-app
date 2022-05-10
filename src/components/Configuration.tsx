1fee431dbfdd1659f5ca4c0bc5a15ba4fdc00639
Johan Dahl <johan.dahl.92@hotmail.com>
2022-05-10 23:22:08 +0200

    pushes changes to server

--- src/components/Configuration.tsx
+++ src/components/Configuration.tsx
@@ -5,7 +5,7 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
 import { IOSSwitch } from './Switch';
 import { AlarmConfig, getAlarm, setAlarm } from '../clients/Alarm'
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
 
 export const Configuration = () => {
   const [time, setTime] = useState(new Date())
@@ -17,38 +17,38 @@ export const Configuration = () => {
     }
   });
 
+  const { mutate } = useMutation(setAlarm)
+  
   useEffect(() => {
-    console.log('NEW CONFIG', config)
-    // useQuery('alarm', setAlarm(config))
-    // store to server
+    config && mutate(config)
     setTime(config && config.timestamp.setHours(parseInt(config.time.split(':')[0]), parseInt(config.time.split(':')[1])) as any)
-  }, [config])
+  }, [config, mutate])
 
   return (
     <>
-    { isLoading || !config ? <div>Loading...</div> : <>
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <TimePicker
-          label="Alarm"
-          value={time}
-          ampm={false}
-          onChange={(date: string | null) => {
-            setConfig({
-              ...config,
-              timestamp: new Date(),
-              time: (date as any).toLocaleTimeString('sv-SE', { hour: '2-digit', minute: '2-digit' })
-            })
-          }}
-          renderInput={(params: any) => <TextField {...params} />}
-        />
-      </LocalizationProvider>
-      <IOSSwitch 
-        checked={config.active} 
-        onClick={() => {
-          setConfig({ ...config, active: !config.active })
-        }}/>
-    </> }
-    
+      { isLoading || !config ? <div>Loading...</div> : <>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <TimePicker
+            label="Alarm"
+            value={time}
+            ampm={false}
+            onChange={(date: string | null) => {
+              setConfig({
+                ...config,
+                timestamp: new Date(),
+                time: (date as any).toLocaleTimeString('sv-SE', { hour: '2-digit', minute: '2-digit' })
+              })
+            }}
+            renderInput={(params: any) => <TextField {...params} />}
+          />
+        </LocalizationProvider>
+        <IOSSwitch 
+          checked={config.active} 
+          onClick={() => {
+            setConfig({ ...config, active: !config.active })
+          }}/>
+        </> 
+      }
     </>
   );
 }
